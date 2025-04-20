feb9b563ece11e1cc1ec02e62da6f68471bbd6c1
Edward Ha <quocvietha08@gmail.com>
2025-04-20 13:34:56 +0700

    update opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,17 @@ import '@mantine/core/styles.css';
 import React from 'react';
 import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
-import './global.css'
+
+import './global.css';
 import '@mantine/notifications/styles.css';
+
+import { Analytics } from '@vercel/analytics/react';
 import { Notifications } from '@mantine/notifications';
 
 export const metadata = {
   title: 'LeetCode Learn - Solve Coding Problems with AI Assistance',
-  description: 'Paste your LeetCode problem link and get instant solutions, explanations, and learning resources. Master algorithms and ace your technical interviews with LeetCode Learn.',
+  description:
+    'Paste your LeetCode problem link and get instant solutions, explanations, and learning resources. Master algorithms and ace your technical interviews with LeetCode Learn.',
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -18,6 +22,14 @@ export default function RootLayout({ children }: { children: any }) {
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
@@ -25,9 +37,10 @@ export default function RootLayout({ children }: { children: any }) {
       </head>
       <body>
         <MantineProvider theme={theme}>
-        <Notifications />
+          <Notifications />
           {children}
-          </MantineProvider>
+        </MantineProvider>
+        <Analytics />
       </body>
     </html>
   );
