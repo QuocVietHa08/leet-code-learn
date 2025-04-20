9319957c179ae62323437d3f5fdb01e32a29272f
Edward Ha <quocvietha08@gmail.com>
2025-04-20 13:28:20 +0700

    Fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import { LandingPage } from '../components/LandingPage';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
-const links = [
-  { link: '#', label: 'Home' },
-  { link: '#', label: 'Features' },
-  { link: '#', label: 'Demo' },
-  { link: '#', label: 'Contact' },
-];
+// const links = [
+//   { link: '#', label: 'Home' },
+//   { link: '#', label: 'Features' },
+//   { link: '#', label: 'Demo' },
+//   { link: '#', label: 'Contact' },
+// ];
 
 export default function HomePage() {
   return (
     <>
-      <Header links={links} />
+      <Header />
       <LandingPage />
-      <Footer links={links} />
+      <Footer />
     </>
   );
 }
